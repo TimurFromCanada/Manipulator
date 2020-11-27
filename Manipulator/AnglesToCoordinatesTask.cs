2b01e190cc86a490ec07ad9ef410a5d420d79d6b
TimurFromCanada <timurfromcanada@gmail.com>
2020-11-27 12:19:26 +0500

    Add files via upload

---
+++ Manipulator/AnglesToCoordinatesTask.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Drawing;
+using NUnit.Framework;
+
+namespace Manipulation
+{
+    public static class AnglesToCoordinatesTask
+    {
+        public static PointF[] GetJointPositions(double shoulder, double elbow, double wrist)
+        {
+            var positionX = Manipulator.UpperArm * (float)Math.Cos(shoulder);
+            var positionY = Manipulator.UpperArm * (float)Math.Sin(shoulder);
+            var elbowPos = new PointF(positionX, positionY);
+
+            positionX += Manipulator.Forearm * (float)Math.Cos(elbow + shoulder - Math.PI);
+            positionY += Manipulator.Forearm * (float)Math.Sin(elbow + shoulder - Math.PI);
+            var wristPos = new PointF(positionX, positionY);
+
+            positionX += Manipulator.Palm * (float)Math.Cos(wrist + elbow + shoulder - 2 * Math.PI);
+            positionY += Manipulator.Palm * (float)Math.Sin(wrist + elbow + shoulder - 2 * Math.PI);
+            var palmEndPos = new PointF(positionX, positionY);
+
+            return new PointF[]
+            {
+                elbowPos,
+                wristPos,
+                palmEndPos
+            };
+        }
+    }
+
+    [TestFixture]
+    public class AnglesToCoordinatesTask_Tests
+    {
+        [TestCase(Math.PI / 2, Math.PI / 2, Math.PI, Manipulator.Forearm + Manipulator.Palm, Manipulator.UpperArm)]
+        public void TestGetJointPositions(double shoulder, double elbow, double wrist, double palmEndX, double palmEndY)
+        {
+            var joints = AnglesToCoordinatesTask.GetJointPositions(shoulder, elbow, wrist);
+            Assert.AreEqual(palmEndX, joints[2].X, 1e-5, "palm endX");
+            Assert.AreEqual(palmEndY, joints[2].Y, 1e-5, "palm endY");
+        }
+    }
+}
