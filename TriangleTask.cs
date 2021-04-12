69d65d0d1f4cfdf30b0fce7dde4a59c27c124839
timurfromcanada <timurfromcanada@gmail.com>
2021-04-12 13:53:24 +0500

    TriangleTask.cs

--- TriangleTask.cs
+++ TriangleTask.cs
@@ -1,28 +1,32 @@
 using System;
+using static System.Math;
 using NUnit.Framework;
 
 namespace Manipulation
 {
     public class TriangleTask
     {
-        /// <summary>
-        /// Возвращает угол (в радианах) между сторонами a и b в треугольнике со сторонами a, b, c 
-        /// </summary>
         public static double GetABAngle(double a, double b, double c)
         {
-            return 0.0;
+            var cosinus = (a * a + b * b - c * c) / (2 * a * b);
+            return (a + b >= c && a + c >= b && b + c >= a)
+                    ? Acos(cosinus)
+                    : double.NaN;
         }
     }
 
     [TestFixture]
     public class TriangleTask_Tests
     {
-        [TestCase(3, 4, 5, Math.PI / 2)]
-        [TestCase(1, 1, 1, Math.PI / 3)]
-        // добавьте ещё тестовых случаев!
+        [TestCase(3, 4, 5, PI / 2)]
+        [TestCase(1, 1, 1, PI / 3)]
+        [TestCase(0, 0, 0, double.NaN)]
+        [TestCase(-1, 3, 4, double.NaN)]
+        [TestCase(1, 1, 1000, double.NaN)]
         public void TestGetABAngle(double a, double b, double c, double expectedAngle)
         {
-            Assert.Fail("Not implemented yet");
+            Assert.AreEqual(expectedAngle, TriangleTask.GetABAngle(a, b, c), 1e+10);
         }
     }
 }
+
